078a767f3065eada323cc174a3e4062ed3867380
JB Pinalie <2850825+jybp@users.noreply.github.com>
2023-07-15 13:18:21 +0200

    fix folder name

--- internal/github/github.go
+++ internal/github/github.go
@@ -8,6 +8,45 @@ import (
 	"github.com/google/go-github/v53/github"
 )
 
+// pull_request / pull_request_review event payload.
+type actionPayload struct {
+	Action      string `json:"action"` // "closed", "submitted"
+	PullRequest struct {
+		Links struct {
+			HTML struct {
+				Href string `json:"href"` // "https://github.com/slack-emoji-reaction/public-repo/pull/1"
+			} `json:"html"`
+		} `json:"_links"`
+		Base struct {
+			Repo struct {
+				Name  string `json:"name"` // "public-repo"
+				Owner struct {
+					HTMLURL string `json:"html_url"` // "https://github.com/slack-emoji-reaction"
+					Login   string `json:"login"`
+				} `json:"owner"`
+			} `json:"repo"`
+		} `json:"base"`
+		// Draft   bool   `json:"draft"`
+		HTMLURL string `json:"html_url"` // "https://github.com/slack-emoji-reaction/public-repo/pull/1"
+		Number  int    `json:"number"`   // 1
+		// RequestedReviewers []any  `json:"requested_reviewers"`
+		// RequestedTeams     []any  `json:"requested_teams"`
+		State string `json:"state"` // "open"
+	} `json:"pull_request"`
+	Repository struct {
+		FullName string `json:"full_name"` // "slack-emoji-reaction/public-repo"
+		Name     string `json:"name"`      // "public-repo"
+		Owner    struct {
+			Login string `json:"login"` // "slack-emoji-reaction"
+		} `json:"owner"`
+	} `json:"repository"`
+
+	// Only present for "pull_request_review". "action" is "submitted".
+	// Review struct {
+	// 	State string `json:"state"` // "commented"
+	// } `json:"review"`
+}
+
 type API struct {
 	client *github.Client
 }
